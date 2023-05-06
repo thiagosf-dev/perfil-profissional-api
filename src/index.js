6872b771a8e37ea566232eea5cd4c8c0a7ef4e64
Thiago Soares Figueiredo <thiagosf.dev@gmail.com>
2023-05-06 12:17:28 -0300

    ajustes de código

--- src/index.js
+++ src/index.js
@@ -1,33 +1,79 @@
 // 1º importo a biblioteca do express e salvo em uma variável
 const express = require('express')
 
-// 2º eu instancio o express e salvo essa intanciação em uma variável
+// 2º instancio o express e salvo essa intanciação em uma variável
 const app = express()
 
 // criando uma variável com o número da porta
 const port = 3000
 
-// 3º adicionou ou registro uma rota no express
-app.get('/clientes', (req, res) => {
-  console.log('teste')
+app.use(express.json())
+app.use(express.urlencoded());
 
-  // consulto no banco todos os clientes
-  const clientes = [
-    {
-      id: 1,
-      nome: 'Cliente 1'
-    },
-    {
-      id: 2,
-      nome: 'Cliente 2'
-    }
-  ]
-
-  res.send(clientes)
+// 3º adiciono ou registro uma rota no express
+app.get('/', (req, res) => {
+  res.send("API Perfil Profissional")
 })
 
-// 4º colocar o express (servidor) para funcionar e rodar na porta 3000 e ficar escutando qualquer ação no brownser
+// ROTAS DE PERFIL
+let geradorId = 1
+let perfis = []
+
+app.get('/perfil', (req, res) => {
+  res.json(perfis.length > 5 ? perfis.slice(perfis.length - 5) : perfis)
+})
+
+app.get('/perfil/:id', (req, res) => {
+  let perfilId = req.params.id
+  let perfilEncontrado = perfis.find((perfil) => perfil.id == perfilId)
+  if (!perfilEncontrado) res.status(400).json({ message: "Erro ao buscar perfil" })
+  res.json(perfilEncontrado)
+})
+
+app.post('/perfil', (req, res) => {
+  let novoPerfil = req.body
+  if (novoPerfil) {
+    novoPerfil.id = geradorId
+    perfis.push(novoPerfil)
+    geradorId++
+    return res.json(novoPerfil)
+  }
+  else return res.status(400).json({ message: "Erro ao cadastrar perfil" })
+})
+
+app.put('/perfil/:id', (req, res) => {
+  let perfilId = req.params.id
+
+  let perfilEncontrado = perfis.findIndex((perfil) => perfil.id == perfilId)
+  if (perfilEncontrado === -1) return res.status(404).json({ message: "Erro ao buscar perfil" })
+
+  let novoPerfil = req.body
+  if (!novoPerfil) return res.status(400).json({ message: "Erro ao editar perfil" })
+
+  perfis[perfilEncontrado] = { ...perfis[perfilEncontrado], ...novoPerfil }
+  return res.json(perfis[perfilEncontrado])
+})
+
+app.post('/perfil/conexao', (req, res) => {
+  let info = req.body
+  if (!info || !info.remetente || !info.destinatario) return res.status(400).json({ message: "Erro ao criar conexão" })
+
+  let remetenteId = info.remetente
+  let remetente = perfis.find(perfil => perfil.id === remetenteId)
+  if (!remetente) return res.status(404).json({ message: "Erro ao buscar remetente" })
+
+  let destinatarioId = info.destinatario
+  let destinatario = perfis.find(perfil => perfil.id === destinatarioId)
+  if (!destinatario) return res.status(404).json({ message: "Erro ao buscar destinatário" })
+
+  remetente.conexoes.push(destinatarioId)
+  destinatario.conexoes.push(remetenteId)
+
+  return res.json({ message: "Conexão criada com sucesso" })
+})
+
+// 4º coloco o express (servidor) para funcionar e rodar na porta 3000 e ficar escutando qualquer ação no brownser
 app.listen(
   port,
-  () => console.log(`Servidor rodando na porta: ${port}`)
+  () => console.log(`Servidor rodando na porta ${port}...`)
 )
